8ca0cf5f0baf481faac119db85c757ebe39f96f1
Paul Campbell <paul@rslw.com>
2012-07-09 10:45:35 +0100

    add unique scope

--- app/models/notches/date.rb
+++ app/models/notches/date.rb
@@ -3,4 +3,6 @@ class Notches::Date < ActiveRecord::Base
 
   has_many :hits, :class_name => "Notches::Hit",
                   :foreign_key => 'notches_date_id'
+
+  scope :unique, group("notches_url_id, notches_session_id, notches_ip_id")
 end
